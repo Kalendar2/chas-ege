7187ca3a9b7e1e46dcd8cb90f84161cc8c5b25e0
Алендарь Сергей <sergey.alendar@mail.ru>
2024-11-05 23:53:47 +0300

    Файл 506352.js изменён

--- zdn/matege2024b/2/506352.js
+++ zdn/matege2024b/2/506352.js
@@ -1,5 +1,4 @@
-let rand = sluchch(0, 10);
-let m = 'масса';
+let weight = 'масса';
 let a;
 let b;
 let c;
@@ -9,22 +8,11 @@ let small = ['монеты', 'малины', 'клубники', 'вилки', '
 let medium = ['коляски', 'собаки', 'кресла', 'холодильника', 'человека', 'шкафа'];
 let big = ['машины', 'морского ската', 'бегемота', 'носорога', 'индийского слона', 'касатки', 'грузовика', 'африканского слона', 'автобуса'];
 let array1 = [];
-let ind = [0, 1, 2, 3];
-let ind2 = [0, 1, 2, 3];
+let index1 = [0, 1, 2, 3];
+let index2 = [0, 1, 2, 3];
 
-function shuffle(array) {
-    let currentIndex = array.length;
-    while (currentIndex != 0) {
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]
-        ];
-    }
-}
-
-shuffle(ind);
-shuffle(ind2);
+index1.shuffle();
+index2.shuffle();
 
 let array3 = [];
 let v = sluchch(0, verysmall.length - 1);
@@ -42,25 +30,19 @@ array3[1] = small[s];
 array3[2] = medium[me];
 array3[3] = big[bi];
 
-let i = 0;
-while (i != 4) {
-    array1[i] = array3[ind2[i]];
-    i++;
-}
+array1 = index2.map(index => array3[index]);
 
 let ans = [];
 for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-        if (ind2[i] == ind[j]) {
-            ans[i] = j;
-        }
-    }
+	for (let j = 0; j < 4; j++) {
+		if (index2[i] == index1[j]) {
+			ans[i] = j;
+		}
+	}
 }
 
-i = 4;
-while (i != 0) {
-    i--;
-    ans[i] = ans[i] + 1;
+for (let i = 3; i >= 0; i--) {
+	ans[i] = ans[i] + 1;
 }
 
 let array = [a + ' мг', b + ' г', c + ' кг', d + ' т'];
@@ -68,7 +50,9 @@ let array = [a + ' мг', b + ' г', c + ' кг', d + ' т'];
 let answer = 'A) ' + ans[0] + ' B) ' + ans[1] + ' C) ' + ans[2] + ' D) ' + ans[3]
 
 NAtask.setTask({
-    text: ' ' + ' A) ' + m + ' ' + array1[0] + ' B) ' + m + ' ' + array1[1] + ' C) ' + m + ' ' + array1[2] + ' D) ' + m + ' ' + array1[3] + '$$ $$' +
-        '1) ' + ' ' + array[ind[0]] + ' 2) ' + ' ' + array[ind[1]] + ' 3) ' + ' ' + array[ind[2]] + ' 4) ' + ' ' + array[ind[3]],
-    answers: answer,
+	text: ' ' + ' A) ' + weight + ' ' + array1[0] + ' B) ' + weight + ' ' + array1[1] + ' C) ' + weight + ' ' + array1[2] +
+		' D) ' + weight + ' ' + array1[3] + '$$ $$' +
+		'1) ' + ' ' + array[index1[0]] + ' 2) ' + ' ' + array[index1[1]] + ' 3) ' + ' ' + array[index1[2]] + ' 4) ' + ' ' +
+		array[index1[3]],
+	answers: answer,
 });
