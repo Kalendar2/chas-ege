635319fcb7f65605139567c70def9869125b76c4
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2024-12-07 22:28:26 +0300

    Update 111557.js

--- zdn/matoge2024/9/111557.js
+++ zdn/matoge2024/9/111557.js
@@ -6,12 +6,12 @@
         let x=v/n;
 	genAssert(x>0,'Скорость не может быть отрицательной'); //Заготовочка!
 	genAssertZ1000(x,'Скорость не должна быть слишком дробной'); //Заготовочка!
-        let the_activeFloatingVehicle = sklonlxkand(["лодка","байдарка","баржа","яхта","моторная лодка"].iz());
+        let the_activeFloatingVehicle = decor.activeFloatingVehicleF.iz();
 	let the_humanSettlementDestination = sklonlxkand(["пункт","город"].iz()); 
 	let the_orderToFind = decor.orderToFind.iz(); // ["найдите","определите","вычислите"]
         NAtask.setTask({
 		text:
-			'' + the_activeFloatingVehicle.ie.toZagl() +' прошла против течения реки ' + s +' км и вернулась в ' + the_humanSettlementDestination.ie +
+			the_activeFloatingVehicle.ie.toZagl() +' прошла против течения реки ' + s +' км и вернулась в ' + the_humanSettlementDestination.ie +
 			' отправления, затратив на обратный путь на ' + chislitlx(n, 'час') +' меньше. ' + the_orderToFind.toZagl() +' скорость '+
 			the_activeFloatingVehicle.re + ' в неподвижной воде, если скорость течения равна '  + a + ' км/ч. Ответ дайте в км/ч.',
 		answers: x,
