86f99cbd54f3637ca5508f2197a2aa9b21d4af05
Алендарь Сергей <sergey.alendar@mail.ru>
2025-01-06 17:41:00 +0300

    Файл 485935.js изменён

--- zdn/matege2024p/12/485935.js
+++ zdn/matege2024p/12/485935.js
@@ -9,37 +9,26 @@
 			}, {
 				r: 'n1*n3 - n2*n3',
 				l: '(n1-n2)*n3'
-			}, ]);
-		}
+			}]);
+		};
+
 		var v = sl1();
-		var s = sl1();
-		var znak = (v ? '+' : '-');
 		var l1 = sl1();
+		var znak = (v ? '+' : '-');
 		var l = sl(0, 4)
 		var chislo = [-1, -0.5, 0, 0.5, 1];
 		var abroad = sl(2, 5).pm();
-		var l2 = (l1 + 1) % 2;
-		var l3 = sl(0, 2);
 		var sc = ['sin', 'cos'];
-		var pm = ['', '-'];
-		var mass = [pm[l1] + sc[l2] + '(n1+ pi/2)', pm[l2] + sc[l2] + '(n1+ 3pi/2)',
-			pm[l2] + sc[l2] + '(n1- pi/2)', pm[l1] + sc[l2] + '(n1- 3pi/2)'
-		];
+
+
 		var otvetsin = ['\\frac{\\pi}{2}', '\\frac{\\pi}{6}', '\\pi', '\\frac{5 \\pi}{6}', '\\frac{3\\pi}{2}'];
 		var otvetcos = ['', '\\frac{\\pi}{3}', '\\frac{\\pi}{2}', '\\frac{2 \\pi}{3}', '\\pi'];
-		var otvet;
-		if (l1) {
-			otvet = otvetcos;
-		} else {
-			otvet = otvetsin;
-		}
+		var otvet = l1 ? otvetcos : otvetsin;
 
 		var mult1 = math.parse(sc[l1] + '(x) +' + chislo[l]);
 		var mult2 = math.parse(sc[l1] + '(x) + ' + abroad);
-		var a = sl(2, 9);
-		var b = sl(2, 9);
-		a = String(a);
-		b = String(b);
+
+		var a = String(sl(2, 9));
 
 		var prod = new math.OperatorNode('*', 'multiply', [mult1, mult2]);
 		var e2 = math.simplify(prod, [{
@@ -58,20 +47,21 @@
 			l: '(n1+n2)*n3'
 		}]);
 
-
 		chas2.task.setTask({
-			text: "$$" + e7.toTex() + '=0' +"$$",
-			analys: 
-				'$$' + e7.toTex() + '=0' + '$$' +
+			text: 'Решите уравнение: ' + "$$" + e7.toTex() + '=0' + "$$",
+			analys: '$$' + e7.toTex() + '=0' + '$$' +
 				'$$' + e5.toTex() + '=0' + '$$' +
 				'$$' + e4.toTex() + '=0' + '$$' +
 				'$$' + e2.toTex() + '=0' + '$$' +
-				'$$\\quad \\' + sc[l1] + ' x +' + chislo[l] + '=0 \\quad \\' + sc[l1] + ' x +' + abroad + '=0 $$' +
+				'$$\\quad \\' + sc[l1] + ' x +' + chislo[l] + '=0 \\quad \\' +
+				sc[l1] + ' x +' + abroad + '=0 $$' +
 				'$$x= \\pm' + otvet[l] + '+2 \\pi k,\\quad k \\in \\mathbb {Z}' + '$$',
 			answers: sc[l1] + ' + ' + chislo[l] + '=0 ' + sc[l1] + ' + ' + abroad + '=0',
 		});
+
 		return true;
 	});
 })();
 //485935
+
 
