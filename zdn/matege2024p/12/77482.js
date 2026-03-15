d69feb44aaa18b22ca93ac71cd835e7288811254
Алендарь Сергей <sergey.alendar@mail.ru>
2026-03-15 17:05:25 +0300

    Файл добавлен в соответсвующую папку

--- zdn/matege2024p/12/77482.js
+++ zdn/matege2024p/12/77482.js
@@ -2,19 +2,17 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 96);
-		let b = a + sl(1, 3);
-		let left = ['0', b - sl(0.5, 5, 0.5)].iz();
-		let right = b + sl(0.5, 5, 0.5);
+		let left = a - sl(0.5, 5, 0.5);
+		let right = a + sl(0.5, 5, 0.5);
 		let sign = [' ', '-'].iz();
 		NAtask.setMinimaxFunctionTask({
-			expr: sign + '(' + ['x', b].shuffle().join('-') + ')^2*e^(' + ['x', a].shuffle().join('-') + ')',
+			expr: sign + '(' + ['x', a].shuffle().join('-') + ')^2*e^(' + ['x', a].shuffle().join('-') + ')',
 			leftEnd: left + '',
 			rightEnd: right + '',
-			forbiddenAnswers: [0, '-0'],
 			primaryStep: 0.01,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
 })();
-//77483
+//77482
